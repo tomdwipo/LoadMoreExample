f2abd99554a5166cadaa3870c3264681da365296
tommy <tommyputranto1@gmail.com>
2016-01-27 21:51:43 +0700

    tes2

---
+++ app/src/main/java/com/example/tommy/myapplication/listener/OnLoadMoreListener.java
@@ -0,0 +1,9 @@
+package com.example.tommy.myapplication.listener;
+
+/**
+ * Created by tommy on 1/27/16.
+ */
+public interface OnLoadMoreListener {
+
+    void onLoadMore();
+}
---
+++ app/src/main/java/com/example/tommy/myapplication/model/User.java
@@ -0,0 +1,25 @@
+package com.example.tommy.myapplication.model;
+
+/**
+ * Created by tommy on 1/27/16.
+ */
+public class User {
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
